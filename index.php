2d70f78b6cc9c14c288113a88e4041d79508b38a
David Fevig <dafevig@gmail.com>
2015-01-15 13:28:56 -0700

    test borders removed placeholder p

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
+
+</html>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8";>
+		<link type="text/css" rel="stylesheet" href="style.css"/>
 		<title>David Fevig</title>
-		<link rel="stylesheet" type[="text/css" href="style.css">
 	</head>
 	<body>
-		<header>
-			<h1>David Fevig</h1>
-			<p>This is the start of my website</p>
-		</header>
-		<p>
-			Run a manual sweep of anomalous airborne or electromagnetic readings. Radiation levels in our atmosphere have increased by 3,000 percent. Electromagnetic and subspace wave fronts approaching synchronization. What is the strength of the ship's deflector shields at maximum output? The wormhole's size and short period would make this a local phenomenon. Do you have sufficient data to compile a holographic simulation?
-		</p>
-		<p>
-			We're acquainted with the wormhole phenomenon, but this... Is a remarkable piece of bio-electronic engineering by which I see much of the EM spectrum ranging from heat and infrared through radio waves, et cetera, and forgive me if I've said and listened to this a thousand times. This planet's interior heat provides an abundance of geothermal energy. We need to neutralize the homing signal.
-		</p>
-
-
+		<div id="header"><h1>David Fevig</h1><p>This is the start of my website</p></div>
+		<div class="left"></div>
+		<div class="right"></div>
+		<div id="footer"></div>
 	</body>
 </html>
